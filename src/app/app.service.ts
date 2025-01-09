70eb241b9063b0bae7b49c498472fccd847cea8a
Srinivas P <srnvs0708@gmail.com>
2025-01-09 17:25:10 +0530

    Updated github token

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,9 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
-const TOKEN =
-  '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';
-
+import { env } from '../../environments/env';
 @Injectable({
   providedIn: 'root',
 })
@@ -11,7 +8,7 @@ export class AppService {
   constructor(private http: HttpClient) {}
 
   getGithubUser(username: string) {
-    const headers = { Authorization: `Bearer ${TOKEN}` };
+    const headers = { Authorization: `Bearer ${env.githubAuthToken}` };
     return this.http.get(`https://api.github.com/users/${username}`, {
       headers,
     });
